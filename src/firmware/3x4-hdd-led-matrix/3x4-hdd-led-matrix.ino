c6525aa208220fa3f98cd19f1e0c7a0e29be8205
Bruno Crema Ferreira <eng.brn@gmail.com>
2023-06-24 19:34:01 -0300

    INO firmware

---
+++ src/firmware/3x4-hdd-led-matrix/3x4-hdd-led-matrix.ino
@@ -0,0 +1,80 @@
+#define ROWS 3
+#define COLS 4
+#define FRAME_TIME 100 // in milliseconds
+
+int cathodes[ROWS] = { 2, 4, 6 };
+int anodes[COLS]   = { 3, 5, 7, 9 };
+int slide = 0;
+
+bool mx_data_slide_animation[ROWS][8] = {
+  {1, 0, 1,   0, 1, 0},
+  {0, 1, 0,   1, 0, 1},  
+  {1, 0, 1,   0, 1, 0}
+};
+
+bool mx[ROWS][COLS] = {};
+unsigned long et = 0;
+
+void setup()
+{
+  // Pin initialization
+  for ( int i = 0; i < ROWS; ++i )
+  {
+    pinMode( cathodes[i], OUTPUT );
+    digitalWrite( cathodes[i], HIGH );
+  }
+  for ( int i = 0; i < COLS; ++i )
+  {
+    pinMode( anodes[i], OUTPUT );
+    digitalWrite( anodes[i], LOW );
+  }
+  
+  // Initialize the output array
+  for (int i = 0; i < ROWS ; ++i)  
+  {
+    for (int j = 0; j < COLS ; ++j) 
+    {
+      mx[i][j] = 0;
+    }
+  }
+  
+  // Initial elapsed time
+  et = millis();
+}
+ 
+void loop()
+{
+  // Store the animation data frame
+  if (et + FRAME_TIME <= millis()) 
+  {
+    for (int i = 0; i < ROWS ; i++)  
+    {
+      for (int j = 0; j < COLS ; j++) {
+        mx[i][j] = mx[i][j + slide];
+      }
+    }
+    
+    // Slide control
+    if (slide < 8)
+      ++slide;
+    else 
+      slide = 0;
+    
+    // Getting elapsed time
+    et = millis();
+  }
+ 
+  // Rendering on physical led matrix
+  for ( int i = 0; i < ROWS; i++ )
+  {
+    digitalWrite( cathodes[i], LOW );
+    
+    for ( int j = 0; j < COLS; j++ )
+      digitalWrite( anodes[j], mx[i][j] );
+    
+    for ( int j = 0; j < COLS; j++ )
+      digitalWrite( anodes[j], LOW );
+      
+    digitalWrite( cathodes[i], HIGH );
+  }
+}
