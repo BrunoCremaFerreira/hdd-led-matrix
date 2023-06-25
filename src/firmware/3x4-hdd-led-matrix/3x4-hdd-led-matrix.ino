ad1f9b9821c96209434edb622489ce889ec33245
Bruno Crema Ferreira <eng.brn@gmail.com>
2023-06-25 04:00:46 -0300

    Firmware update

--- src/firmware/3x4-hdd-led-matrix/3x4-hdd-led-matrix.ino
+++ src/firmware/3x4-hdd-led-matrix/3x4-hdd-led-matrix.ino
@@ -1,41 +1,83 @@
+/*  3x4 Hdd Led Matrix Project
+ *  (C) 2023 Bruno Crema Ferreira
+ *  This code is licenced under the MIT License.
+ */
+
 #define ROWS 3
 #define COLS 4
-#define FRAME_TIME 500 // in milliseconds
+#define FRAME_TIME 90 // in milliseconds
 
 int cathodes[ROWS] = { 2, 4, 6 };
 int anodes[COLS]   = { 3, 5, 7, 9 };
 int slide = 0;
 
-bool mx_data_slide_animation[ROWS][4] = {
+bool mx_animation_1[ROWS][COLS] = {
   {1, 0, 1, 0},
   {0, 1, 0, 1},  
   {1, 0, 1, 0}
 };
 
-bool mx[ROWS][COLS] = {};
-unsigned long et = 0;
+bool mx_animation_2[ROWS][COLS] = {
+  {1, 0, 0, 0},
+  {1, 0, 0, 0},  
+  {1, 0, 0, 0}
+};
 
-void run_slide_animation()
+bool mx[ROWS][COLS] = {};
+
+void clear()
 {
-  // Store the animation data frame
-  if (et + FRAME_TIME <= millis()) 
-  {
-    for (int i = 0; i < ROWS ; i++)  
-    {
-      for (int j = 0; j < COLS ; j++) {
-        mx[i][j] = mx[i][j + slide];
-      }
+  for (int i = 0; i < ROWS ; ++i) {
+    for (int j = 0; j < COLS ; ++j) {
+      mx[i][j] = 0;
     }
-    
-    // Slide control
-    if (slide < 4)
-      ++slide;
-    else 
-      slide = 0;
-    
-    // Getting elapsed time
-    et = millis();
   }
+}
+
+void run_animation_1()
+{
+  for (int i = 0; i < ROWS ; ++i) {
+    for (int j = 0; j < COLS ; ++j) {
+      mx[i][j] = mx_animation_1[i][j + slide];
+    }
+  }
+
+  if(slide < COLS)
+    slide++;
+  else
+    slide = 0;
+}
+
+void run_animation_2()
+{
+  for (int i = 0; i < ROWS ; ++i) {
+    for (int j = 0; j < COLS ; ++j) {
+      mx[i][j] = mx_animation_1[i][j + slide];
+    }
+  }
+
+  if(slide < COLS)
+    slide++;
+  else
+    slide = 0;
+}
+
+void run_animation_blink_all()
+{
+  slide++;
+  if (slide < 10) {
+    clear();
+    return;
+  }
+
+  for (int i = 0; i < ROWS ; ++i) {
+    for (int j = 0; j < COLS ; ++j) {
+      mx[i][j] = 1;
+    }
+  }
+
+  if(slide > 20)
+    slide = 0;
 }
 
 void setup()
@@ -51,33 +93,31 @@ void setup()
   }
   
   // Initialize the output array
-  for (int i = 0; i < ROWS ; ++i) {
-    for (int j = 0; j < COLS ; ++j) {
-      mx[i][j] = 0;
-    }
-  }
-  
-  // Initial elapsed time
-  et = millis();
+  clear();
 }
  
 void loop()
 {
-  run_slide_animation();
+  delay(FRAME_TIME);
+
+  run_animation_1();
  
   // Rendering on physical led matrix
   for ( int i = 0; i < ROWS; i++ )
   {
     digitalWrite( cathodes[i], LOW );
-    
+    delay(2);
+
     for ( int j = 0; j < COLS; j++ ) {
       digitalWrite( anodes[j], mx[i][j] );
+      delay(2);
+    }  
+
+    for ( int j = 0; j < COLS; j++ ) {
+      digitalWrite( anodes[j], 0 );
     }
     
-    for ( int j = 0; j < COLS; j++ ) {
-      digitalWrite( anodes[j], LOW );
-    }
-      
     digitalWrite( cathodes[i], HIGH );
+    delay(2);
   }
 }
