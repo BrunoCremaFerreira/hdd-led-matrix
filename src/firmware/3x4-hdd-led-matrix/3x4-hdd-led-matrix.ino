3c76a753e583f32f0bc6e8fcf8ece5b40fe9f1b6
Bruno Crema Ferreira <eng.brn@gmail.com>
2023-06-25 01:58:23 -0300

    Adjusting matrix

--- src/firmware/3x4-hdd-led-matrix/3x4-hdd-led-matrix.ino
+++ src/firmware/3x4-hdd-led-matrix/3x4-hdd-led-matrix.ino
@@ -1,48 +1,21 @@
 #define ROWS 3
 #define COLS 4
-#define FRAME_TIME 100 // in milliseconds
+#define FRAME_TIME 500 // in milliseconds
 
 int cathodes[ROWS] = { 2, 4, 6 };
 int anodes[COLS]   = { 3, 5, 7, 9 };
 int slide = 0;
 
-bool mx_data_slide_animation[ROWS][8] = {
-  {1, 0, 1,   0, 1, 0},
-  {0, 1, 0,   1, 0, 1},  
-  {1, 0, 1,   0, 1, 0}
+bool mx_data_slide_animation[ROWS][4] = {
+  {1, 0, 1, 0},
+  {0, 1, 0, 1},  
+  {1, 0, 1, 0}
 };
 
 bool mx[ROWS][COLS] = {};
 unsigned long et = 0;
 
-void setup()
-{
-  // Pin initialization
-  for ( int i = 0; i < ROWS; ++i )
-  {
-    pinMode( cathodes[i], OUTPUT );
-    digitalWrite( cathodes[i], HIGH );
-  }
-  for ( int i = 0; i < COLS; ++i )
-  {
-    pinMode( anodes[i], OUTPUT );
-    digitalWrite( anodes[i], LOW );
-  }
-  
-  // Initialize the output array
-  for (int i = 0; i < ROWS ; ++i)  
-  {
-    for (int j = 0; j < COLS ; ++j) 
-    {
-      mx[i][j] = 0;
-    }
-  }
-  
-  // Initial elapsed time
-  et = millis();
-}
- 
-void loop()
+void run_slide_animation()
 {
   // Store the animation data frame
   if (et + FRAME_TIME <= millis()) 
@@ -55,7 +28,7 @@ void loop()
     }
     
     // Slide control
-    if (slide < 8)
+    if (slide < 4)
       ++slide;
     else 
       slide = 0;
@@ -63,17 +36,47 @@ void loop()
     // Getting elapsed time
     et = millis();
   }
+}
+
+void setup()
+{
+  // Pin initialization
+  for ( int i = 0; i < ROWS; ++i ) {
+    pinMode( cathodes[i], OUTPUT );
+    digitalWrite( cathodes[i], HIGH );
+  }
+  for ( int i = 0; i < COLS; ++i ) {
+    pinMode( anodes[i], OUTPUT );
+    digitalWrite( anodes[i], LOW );
+  }
+  
+  // Initialize the output array
+  for (int i = 0; i < ROWS ; ++i) {
+    for (int j = 0; j < COLS ; ++j) {
+      mx[i][j] = 0;
+    }
+  }
+  
+  // Initial elapsed time
+  et = millis();
+}
+ 
+void loop()
+{
+  run_slide_animation();
  
   // Rendering on physical led matrix
   for ( int i = 0; i < ROWS; i++ )
   {
     digitalWrite( cathodes[i], LOW );
     
-    for ( int j = 0; j < COLS; j++ )
+    for ( int j = 0; j < COLS; j++ ) {
       digitalWrite( anodes[j], mx[i][j] );
+    }
     
-    for ( int j = 0; j < COLS; j++ )
+    for ( int j = 0; j < COLS; j++ ) {
       digitalWrite( anodes[j], LOW );
+    }
       
     digitalWrite( cathodes[i], HIGH );
   }
