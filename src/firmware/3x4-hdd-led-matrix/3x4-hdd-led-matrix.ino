a6b3f233930e707565ddcd472d78c6d3604afc67
Bruno Crema Ferreira <eng.brn@gmail.com>
2023-06-25 19:38:11 -0300

    Serial read

--- src/firmware/3x4-hdd-led-matrix/3x4-hdd-led-matrix.ino
+++ src/firmware/3x4-hdd-led-matrix/3x4-hdd-led-matrix.ino
@@ -10,17 +10,12 @@
 int cathodes[ROWS] = { 2, 4, 6 };
 int anodes[COLS]   = { 3, 5, 7, 9 };
 int slide = 0;
+int display_mode = 0;
 
-bool mx_animation_1[ROWS][COLS] = {
+bool mx_animation_pattern[ROWS][COLS] = {
   {1, 0, 1, 0},
   {0, 1, 0, 1},  
   {1, 0, 1, 0}
-};
-
-bool mx_animation_2[ROWS][COLS] = {
-  {1, 0, 0, 0},
-  {1, 0, 0, 0},  
-  {1, 0, 0, 0}
 };
 
 bool mx[ROWS][COLS] = {};
@@ -34,25 +29,11 @@ void clear()
   }
 }
 
-void run_animation_1()
+void run_animation()
 {
   for (int i = 0; i < ROWS ; ++i) {
     for (int j = 0; j < COLS ; ++j) {
-      mx[i][j] = mx_animation_1[i][j + slide];
-    }
-  }
-
-  if(slide < COLS)
-    slide++;
-  else
-    slide = 0;
-}
-
-void run_animation_2()
-{
-  for (int i = 0; i < ROWS ; ++i) {
-    for (int j = 0; j < COLS ; ++j) {
-      mx[i][j] = mx_animation_1[i][j + slide];
+      mx[i][j] = mx_animation_pattern[i][j + slide];
     }
   }
 
@@ -80,6 +61,28 @@ void run_animation_blink_all()
     slide = 0;
 }
 
+void read_serial_input()
+{
+  if(Serial.available() <= 0) {
+    return;
+  }
+
+  String str_command = Serial.readStringUntil('\n');
+
+  if(str_command.startsWith("err;")) {
+    display_mode = 1;
+    return;
+  }
+  
+  if(str_command == "disks;"){
+    display_mode = 2;
+    return;
+  }
+
+  // Default
+  display_mode = 0;
+}
+
 void setup()
 {
   // Pin initialization
@@ -94,14 +97,31 @@ void setup()
   
   // Initialize the output array
   clear();
+
+  Serial.begin(9600);
 }
  
 void loop()
 {
-  delay(FRAME_TIME);
+  read_serial_input();
 
-  run_animation_1();
- 
+  switch (display_mode)
+  {
+    // Disk Error animation
+    case 1: 
+      run_animation_blink_all();
+      break;
+    // Show disk position
+    case 2:
+      break;
+    // Default animation
+    default:
+      run_animation();  
+      break;
+  }
+
+  delay(FRAME_TIME);
+  
   // Rendering on physical led matrix
   for ( int i = 0; i < ROWS; i++ )
   {
