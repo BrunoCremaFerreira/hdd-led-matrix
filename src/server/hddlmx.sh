2a539340fc23d94ea872b0bd99e87b55c3fc8ec4
Bruno Crema Ferreira <eng.brn@gmail.com>
2023-06-26 01:53:04 -0300

    Matrix actions functions

--- src/server/hddlmx.sh
+++ src/server/hddlmx.sh
@@ -1,13 +1,55 @@
 #!/bin/sh
 
+export HDD_LED_MATRIX_DEVICE=/dev/ttyACM0
+
 _get_device_names()
 {
     lsblk --output name --nodeps --sort NAME --include 8 -nd
 }
 
+_show_disk_error()
+{
+    stty -F /dev/ttyACM0 -hupcl
+    echo "err:$1" > $HDD_LED_MATRIX_DEVICE
+}
+
+_show_installed_disks()
+{
+    stty -F /dev/ttyACM0 -hupcl
+    echo "disks:$1" > $HDD_LED_MATRIX_DEVICE
+}
+
+_show_animation()
+{
+    stty -F /dev/ttyACM0 -hupcl
+    echo "0" > $HDD_LED_MATRIX_DEVICE
+}
+
+_test_matrix()
+{
+    echo "Simulatind disk error:"
+    echo "  .   *   .   ."
+    echo "  *   .   .   ." 
+    echo "  .   .   *   ."
+    _show_disk_error "0100 1000 0010"
+    sleep 10
+
+    echo "Simulatind disk Installed Position:"
+    echo "  *   .   *   *"
+    echo "  .   *   *   *" 
+    echo "  *   *   .   *"
+    _show_installed_disks "1011 0111 1101"
+    sleep 10
+
+    echo "Simulatind disk normal activiti"
+    _show_animation
+
+    echo "Done."
+}
+
 _main()
 {
-    _get_device_names
+    _test_matrix
 }
 
 _main
